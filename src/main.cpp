3d79dbfbb3593cab8a2eed63a96e9769ee8c7d78
Venkat Raju <venkatapathiraju.t@gmail.com>
2021-07-04 19:38:01 +0100

    async,future

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,27 @@
 #include <iostream>
 #include <thread>
+#include <chrono>
+#include <future>
 
 
+//Use futures, async, promise etc
 
-int main(int argc, char *argv[])
+int delayedaccumulator(int n)
 {
-	
+	std::cout << "Begin of delayed accumulator" << std::endl;
+	using namespace std::chrono_literals;
+	int sum = 0;
+	for(int i=0; i<=n ; i++)
+	{
+		std::this_thread::sleep_for(100ms);
+		sum+=i;
+	}
+	std::cout << "End of delayed accumulator" << std::endl;
+	return sum;
+}
+
+void basicthreadcall()
+{
 	auto sqr = [](int x) { 
 		std::cout << "Square root of " << x << " is " << x*x << std::endl;
 		return x*x; 
@@ -20,6 +36,18 @@ int main(int argc, char *argv[])
 		std::thread t(sqr,i);
 		t.join();
 	}
-	
+}
+
+
+
+int main(int argc, char *argv[])
+{
+
+	//call a function asynchronously
+	auto result = std::async(delayedaccumulator,100);
+
+	std::cout << " Do something else" << std::endl;
+	std::cout << "Got the result " << result.get() << std::endl;
+		
 
 }
