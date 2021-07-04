d504b9802d6dc2063ae0ea33fbf3c2726bb0771c
Venkat Raju <venkatapathiraju.t@gmail.com>
2021-07-04 15:33:40 +0100

    basic

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
+#include <thread>
+
+
 
 int main(int argc, char *argv[])
 {
-	std::cout << "Hello world!" << std::endl;
+	
+	auto sqr = [](int x) { 
+		std::cout << "Square root of " << x << " is " << x*x << std::endl;
+		return x*x; 
+	};
+
+	int cores = std::thread::hardware_concurrency();
+	std::cout << "Hardware cores->" << cores;
+
+	//Run the function with parallelism
+	for(int i =0; i < 1000;i++)
+	{
+		std::thread t(sqr,i);
+		t.join();
+	}
+	
+
 }
